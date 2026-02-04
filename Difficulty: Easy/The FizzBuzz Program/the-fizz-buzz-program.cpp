8fff47c79b084ae4aa4003443a6393091b86de20
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-02-04 22:29:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/The FizzBuzz Program/the-fizz-buzz-program.cpp
+++ Difficulty: Easy/The FizzBuzz Program/the-fizz-buzz-program.cpp
@@ -1,9 +1,13 @@
-// User function Template for C++
+#include <bits/stdc++.h>
+using namespace std;
 
-void fizzBuzz(int number) {
+int main() {
+    int number;
+    cin>>number;
     // Write your code here.
     if(number%3==0 && number%5!=0) cout<<"Fizz"<<"\n";
     else if(number%3==0 && number%5==0) cout<<"FizzBuzz"<<"\n";
     else if(number%5==0 && number%3!=0) cout<<"Buzz"<<"\n";
     else cout<<number<<"\n";
+return 0;
 }
