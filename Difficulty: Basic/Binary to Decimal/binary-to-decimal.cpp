1a8fa4efe00e6c51895107a8161e02feb9aafbb1
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-02-04 22:26:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Binary to Decimal/binary-to-decimal.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int binaryToDecimal(string &b) {
+        // Code here.
+        int decimal = 0;
+
+        for (char bit : b) {
+            decimal = decimal * 2 + (bit - '0');
+        }
+
+        return decimal;
+    }
+};
+ 
