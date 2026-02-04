0945549fa56e34899265db7ce1828ef720c05f4d
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-02-04 22:15:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Arithmetic Operators/arithmetic-operators.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // code here
+    int x;
+    int y;
+    cin>>x >>y;
+    cout<<x+y<<" "<<x-y<<" "<<x*y<<" "<<x/y<<" "<<x%y;
+
+    return 0;
+}
