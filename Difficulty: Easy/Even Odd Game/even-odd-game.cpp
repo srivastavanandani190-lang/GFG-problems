e95e97c8a5f76b5b20f152f50fa12016d6779a11
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-02-05 22:04:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Even Odd Game/even-odd-game.cpp
@@ -0,0 +1,12 @@
+// User function Template for C++
+
+string utility(int n) {
+
+    // write your code here
+    if(n%2==0){
+        return "Friend";
+    }
+    else{
+        return "You";
+    }
+}
