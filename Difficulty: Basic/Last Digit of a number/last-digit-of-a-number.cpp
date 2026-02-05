212bdc26060aad833e8873dd269f0aaad3a0b2fe
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-02-05 09:24:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Last Digit of a number/last-digit-of-a-number.cpp
@@ -0,0 +1,14 @@
+void utility(int n) {
+
+    // just complete below statement
+    int ans =n%10;
+
+        // print the result
+        if(ans<0){
+            cout<<(ans*(-1))<<"\n";
+        }
+        else{
+           cout << ans << endl; 
+        }
+        
+}
