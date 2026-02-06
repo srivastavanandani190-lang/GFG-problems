c71c70f7153ba79ee7ef5e608148677c346b91c4
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-02-06 10:37:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/For Loop - 1/for-loop-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    // use for loop to print the
+    // multiplication table of n
+  for(int i=1;i<=10;i++){
+      cout<<i*n<<" ";
+  }
+    return 0;
+}
