e87eadedb8d784773dc6fb0ee6a046dade1af1c6
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-02-06 10:36:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Function With No Arguments/function-with-no-arguments.cpp
@@ -0,0 +1,8 @@
+// User function Template for C++
+
+// Write the helloFunction below. It should have one statement
+// cout<<"Hello"<<endl;
+//  code here
+void helloFunction(){
+    cout<<"Hello"<<"\n";
+}
