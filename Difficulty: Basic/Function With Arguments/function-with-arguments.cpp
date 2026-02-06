4e1ecb892afd6f6d7e2c7376061f0765b2975585
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-02-06 12:11:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Function With Arguments/function-with-arguments.cpp
@@ -0,0 +1,9 @@
+// User function Template for C++
+
+// Write the complete argumentFunction below.
+// The function should take two arguments a and b
+// The function should return a+b
+// code here
+int argumentFunction(int a,int b){
+    return a+b;
+}
