5f2aae2e56600056470d0a88c3e0ad92ec93c268
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-02-08 19:25:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Design a class/design-a-class.cpp
@@ -0,0 +1,9 @@
+// User function Template for C++
+
+// Design a class as described in the question
+// Code here
+class MyClass{
+   public: void display(){
+        cout<<"Hello World";
+    }
+};
