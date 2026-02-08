b9538159a7cbcea009ce1f31c09dcabc2a222a2d
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-02-08 18:55:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/C++ Switch Case Statement/c-switch-case-statement.cpp
@@ -0,0 +1,52 @@
+class Solution {
+  public:
+    void isInRange(int n) {
+        // you don't need to take input
+        // here just write code which print correct answer according to n
+        switch(n){
+            case 1:
+            cout<<"one"<<"\n";
+            break;
+            
+             case 2:
+            cout<<"two"<<"\n";
+            break; 
+            
+            case 3:
+            cout<<"three"<<"\n";
+            break;
+            
+             case 4:
+            cout<<"four"<<"\n";
+            break;
+            
+             case 5:
+            cout<<"five"<<"\n";
+            break;
+            
+             case 6:
+            cout<<"six"<<"\n";
+            break;
+            
+             case 7:
+            cout<<"seven"<<"\n";
+            break;
+            
+             case 8:
+            cout<<"eight"<<"\n";
+            break;
+            
+            case 9:
+            cout<<"nine"<<"\n";
+            break;
+             
+            case 10:
+            cout<<"ten"<<"\n";
+            break;
+              
+            default:
+            cout<<"not in range"<<"\n";
+            break;
+        }
+    }
+};
