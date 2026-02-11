548f8bf7764ebb2c07feb9b2a5124370c1dd14d9
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-02-11 21:27:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Add - Auto Keyword/add-auto-keyword.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    float b;
+    long c;
+    cin >> a >> b >> c;
+     auto ans=a+b+c;
+    // add these variables into ans variable
+
+    // printing the ans
+    cout << ans << endl;
+    return 0;
+}
