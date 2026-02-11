df1e96c9772a3a571d313528db8f03accd265360
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-02-11 21:33:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/AP Term/ap-term.cpp
+++ Difficulty: Easy/AP Term/ap-term.cpp
@@ -1,7 +1,8 @@
-int apTerm(int a, int d, int n) {
-    // code here
-    
-    int ans =a+(n-1)*d;
-
-        return ans;
-}
+class Solution {
+  public:
+    int nthTerm(int a, int d, int n) {
+        // code here
+        int res=a+(n-1)*d;
+        return res;
+    }
+};
