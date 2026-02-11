42aee5f3f66c0de80cdc889beb9b45d832d72b35
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-02-11 21:30:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/C++ STL | Set 1 (vector)/c-stl-set-1-vector.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* inserts an element x at the back of the vector A */
+void add_to_vector(vector<int> &A, int x) {
+    A.push_back(x);
+}
+
+/* sort the vector A in ascending order */
+void sort_vector_asc(vector<int> &A) {
+    sort(A.begin(), A.end());
+}
+
+/* reverses the vector A */
+void reverse_vector(vector<int> &A) {
+    reverse(A.begin(), A.end());
+}
+
+/* returns the size of the vector A */
+int size_of_vector(vector<int> &A) {
+    return A.size();
+}
+
+/* sorts the vector A in descending order */
+void sort_vector_desc(vector<int> &A) {
+    sort(A.begin(), A.end(), greater<int>());
+}
+
+/* prints space separated elements of vector A */
+void print_vector(vector<int> &A) {
+    for (int x : A) {
+        cout << x << " ";
+    }
+}
