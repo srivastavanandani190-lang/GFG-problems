17ba64dde9045b4e7d3de5e5deb79fe57145ac57
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-02-11 21:21:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/For Loop - 2/for-loop-2.cpp
+++ Difficulty: Basic/For Loop - 2/for-loop-2.cpp
@@ -1,11 +1,11 @@
-void utility(string s) {
-    // length of string is given by s.length()
-    // character at any index i is given by s[i]
-
-    // Write your code here
-    for(int i=0;i<s.size();i++){
-        if(i%2==0){
-            cout<<s[i];
+class Solution {
+  public:
+    void printEvenIndices(const string& s) {
+        // code here
+        for(int i=0;i<s.size();i++){
+            if(i%2==0){
+                cout<<s[i];
+            }
         }
     }
-}
+};
