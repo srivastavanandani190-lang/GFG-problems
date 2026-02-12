41467a36203ac90320656e4cf33dad5a32ae6f3c
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-02-12 10:26:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Compare two numbers/compare-two-numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // code here
+    int a,b;
+    cin>>a;
+    cin>>b;
+    if(a>b){
+    cout<<1;
+    }
+    else{
+        cout<<0;
+    }
+
+    return 0;
+}
