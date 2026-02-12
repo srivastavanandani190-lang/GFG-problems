6da7b26be07103c1068a59fa57e0e8c39241aa06
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-02-12 09:49:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Arithmetic Operators/arithmetic-operators.cpp
+++ Difficulty: Basic/Arithmetic Operators/arithmetic-operators.cpp
@@ -1,12 +1,17 @@
-#include <iostream>
-using namespace std;
+// User function Template for C++
 
-int main() {
-    // code here
-    int x;
-    int y;
-    cin>>x >>y;
-    cout<<x+y<<" "<<x-y<<" "<<x*y<<" "<<x/y<<" "<<x%y;
+void operations(int x, int y) {
+    // Perform addition x+y below
+    int p =x+y;
+        // Perform subtraction x-y below
+        int q =x-y;
+            // Perform multiplication x*y below
+        int r =x*y;
+            // Perform division x/y below
+        int s =x/y;
+            // Perform modulo operation x%y below
+        int t =x%y;
 
-    return 0;
+            // The below code prints the output. Don't change it!
+        cout << p << " " << q << " " << r << " " << s << " " << t << endl;
 }
