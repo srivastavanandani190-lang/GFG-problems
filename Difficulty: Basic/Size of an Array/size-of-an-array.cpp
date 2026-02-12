a2017c2ac3def70855161a3b97873ac31bec76a3
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-02-12 10:01:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Size of an Array/size-of-an-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  private:
+    vector<int> a;
+
+  public:
+    // Constructor to initialize the array
+    Solution(vector<int> arr) { this->a = arr; }
+
+    int getSize() {
+        // code here
+        return a.size();
+    }
+};
