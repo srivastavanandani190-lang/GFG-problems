7bd97b5bba2c0c0c83e9588c49581e5f37bf940b
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-02-12 09:47:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Type Inference/type-inference.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <typeinfo>
+using namespace std;
+
+int main() {
+    int a = 3;
+    float b = 5.57;
+
+    // calculate typeid for variable a and b
+    string id1 = typeid(a).name();
+    string id2 = typeid(b).name();
+
+    // printing the typeid's
+    cout << id1 << " " << id2 << endl;
+
+    return 0;
+}
