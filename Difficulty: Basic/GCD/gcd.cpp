2a4e776d3e2dcf45ae21c59e571bafc4e9d01fb5
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-02-12 09:57:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/GCD/gcd.cpp
+++ Difficulty: Basic/GCD/gcd.cpp
@@ -1,6 +1,17 @@
-int gcd(int a, int b) {
-    if (b == 0) {
+#include <iostream>
+using namespace std;
+int gcd(int a,int b){
+    if(b==0){
         return a;
     }
-    return gcd(b, a % b);
+    return gcd(b,a%b);
+}
+int main() {
+    // code here
+    int a,b;
+    cin>>a;
+    cin>>b;
+    cout<<gcd(a,b);
+
+    return 0;
 }
