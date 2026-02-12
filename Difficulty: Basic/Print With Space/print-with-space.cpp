b6ecfda02088d00412c54274632da6f64bad7fff
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-02-12 09:32:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Print With Space/print-with-space.cpp
+++ Difficulty: Basic/Print With Space/print-with-space.cpp
@@ -1,10 +1,7 @@
 // User function Template for C++
 void utility() {
     string a, b;
-    cin>>a;
-    cin>>b;
+    cin >> a >> b;
     // Write your code below that prints a <space> b
     cout<<a<<" "<<b<<"\n";
-    
-    return ;
 }
