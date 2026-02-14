3e4180e62f6459e26522d914bae952f1eee12167
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-02-14 21:27:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Factorial/factorial.cpp
+++ Difficulty: Basic/Factorial/factorial.cpp
@@ -1,16 +1,15 @@
-class Solution {
-  public:
-    int factorial(int n) {
-        // code here
-        int fact=1;
-        if(n==0 || n==1){
-            return 1;
-        }
-        else{
-            for(int i=1;i<=n;i++){
-                fact*=i;
-            }
-        }
-        return fact;
+// User function Template for C++
+
+int nFactorial(int n) {
+    int ans = 1;
+    
+    if(n==0 || n==1){
+        return 1;
     }
-};
+    // Write your code here
+    for(int i=1;i<=n;i++){
+        ans*=i;
+    }
+
+    return ans;
+}
