473acce76db533b8d05fc697aee012159a811ba8
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-03-19 22:23:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -1,32 +1,13 @@
 class Solution {
-  public:
-    int kthElement(vector<int> &a, vector<int> &b, int k) {
+public:
+    int kthElement(vector<int>& a, vector<int>& b, int k) {
+        vector<int> v;
 
-        int i = 0, j = 0, count = 0;
+        v.insert(v.end(), a.begin(), a.end());
+        v.insert(v.end(), b.begin(), b.end());
 
-        while (i < a.size() && j < b.size()) {
-            count++;
-            if (a[i] <= b[j]) {
-                if (count == k) return a[i];
-                i++;
-            } else {
-                if (count == k) return b[j];
-                j++;
-            }
-        }
+        sort(v.begin(), v.end());
 
-        while (i < a.size()) {
-            count++;
-            if (count == k) return a[i];
-            i++;
-        }
-
-        while (j < b.size()) {
-            count++;
-            if (count == k) return b[j];
-            j++;
-        }
-
-        return -1;
+        return v[k - 1];
     }
 };
