3b6bfc979f62196d8b718d207774c4c6652a8371
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-03-28 15:27:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Zero Converter - Python/zero-converter-python.py
@@ -0,0 +1,10 @@
+def pos(n):
+    ## Write the code
+    for i in range(n-1,-1,-1):
+        print(i,end=" ")
+        
+    
+def neg(n):
+    ##Write the code
+    for i in range(n,1):
+        print(i,end=" ")
