380cdfbcf31233783332dbd00699a5d653cd19b0
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-05-26 20:06:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.cpp
@@ -0,0 +1,44 @@
+class Solution {
+  public:
+    vector<int> spirallyTraverse(vector<vector<int>> &mat) {
+        vector<int> ans;
+        
+        int top = 0;
+        int bottom = mat.size() - 1;
+        int left = 0;
+        int right = mat[0].size() - 1;
+        
+        while(top <= bottom && left <= right) {
+            
+            // left -> right
+            for(int i = left; i <= right; i++) {
+                ans.push_back(mat[top][i]);
+            }
+            top++;
+            
+            // top -> bottom
+            for(int i = top; i <= bottom; i++) {
+                ans.push_back(mat[i][right]);
+            }
+            right--;
+            
+            // right -> left
+            if(top <= bottom) {
+                for(int i = right; i >= left; i--) {
+                    ans.push_back(mat[bottom][i]);
+                }
+                bottom--;
+            }
+            
+            // bottom -> top
+            if(left <= right) {
+                for(int i = bottom; i >= top; i--) {
+                    ans.push_back(mat[i][left]);
+                }
+                left++;
+            }
+        }
+        
+        return ans;
+    }
+};
