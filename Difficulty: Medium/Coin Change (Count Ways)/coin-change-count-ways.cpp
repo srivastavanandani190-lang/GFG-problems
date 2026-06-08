aabd1b91043e304c81fb9880962120e5c29ae066
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-06-08 14:49:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Coin Change (Count Ways)/coin-change-count-ways.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int count(vector<int>& coins, int sum) {
+        vector<int> dp(sum + 1, 0);
+
+        dp[0] = 1;
+
+        for (int coin : coins) {
+            for (int j = coin; j <= sum; j++) {
+                dp[j] += dp[j - coin];
+            }
+        }
+
+        return dp[sum];
+    }
+};
