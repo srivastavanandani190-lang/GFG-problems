b0987782d618fee44c226600340d45a0c3086a02
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-06-10 12:13:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stickler Thief/stickler-thief.cpp
+++ Difficulty: Medium/Stickler Thief/stickler-thief.cpp
@@ -2,23 +2,18 @@ class Solution {
   public:
     int findMaxSum(vector<int>& arr) {
         int n = arr.size();
-        
-        if (n == 0) return 0;
-        if (n == 1) return arr[0];
-        
-        int prev2 = 0;       // dp[i-2]
-        int prev1 = arr[0];  // dp[i-1]
-        
-        for (int i = 1; i < n; i++) {
-            int take = arr[i] + prev2;
-            int notTake = prev1;
-            
-            int curr = max(take, notTake);
-            
-            prev2 = prev1;
-            prev1 = curr;
-        }
-        
-        return prev1;
+       vector<int>dp(n,-1);
+       if(n==1){
+           return arr[0];
+       }
+       dp[0]=arr[0];
+       dp[1]=max(arr[0],arr[1]);
+       
+       for(int i=2;i<n;i++){
+           dp[i]=max(arr[i]+dp[i-2],dp[i-1]);
+       }
+       
+       
+        return dp[n-1];
     }
 };
