e3eff5b16b8e2243f99eac1ffc2807024a62d501
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-06-12 12:41:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
@@ -3,31 +3,24 @@ class Solution {
     bool isBalanced(string& s) {
         stack<char> st;
 
-        for(int i = 0; i < s.size(); i++) {
-            char ch = s[i];
- 
-            if(ch == '(' || ch == '{' || ch == '[') {
-                st.push(ch);
-            }
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } 
             else {
-                
-                if(st.empty()) return false;
+                if (st.empty()) return false;
 
-                char top = st.top();
-
-                
-                if((ch == ')' && top == '(') ||
-                   (ch == '}' && top == '{') ||
-                   (ch == ']' && top == '[')) {
+                if ((c == ')' && st.top() == '(') ||
+                    (c == '}' && st.top() == '{') ||
+                    (c == ']' && st.top() == '[')) {
                     st.pop();
-                }
+                } 
                 else {
                     return false;
                 }
             }
         }
 
-         
         return st.empty();
     }
-};
+}; 
