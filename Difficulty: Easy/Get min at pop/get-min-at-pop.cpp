e0278d6728b42419de2cb6ffcafb234d8f23f615
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-06-12 13:16:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Get min at pop/get-min-at-pop.cpp
@@ -0,0 +1,24 @@
+stack<int> _push(int arr[], int n) {
+    // code here
+    stack<int>st;
+    for(int i=0;i<n;i++){
+        if(i==0){
+            st.push(arr[i]);
+        }
+        else{
+            st.push(min(arr[i],st.top()));
+        }
+    }
+    return st;
+    
+}
+
+// Function to print minimum value in stack each time while popping.
+void _getMinAtPop(stack<int> s) {
+
+    while(!s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+    // code here
+}
