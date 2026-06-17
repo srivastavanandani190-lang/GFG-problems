961c89c141408afafe2d52f7cde4093d7a8ea93e
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-06-17 12:05:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Max of min for every window size/max-of-min-for-every-window-size.cpp
@@ -0,0 +1,44 @@
+class Solution {
+  public:
+    vector<int> maxOfMins(vector<int>& arr) {
+        //  code here
+        int n=arr.size();
+        vector<int>ans(n,0);
+        stack<int>st;
+        for(int i=0;i<n;i++){
+           while(!st.empty() && arr[st.top()]>arr[i]){
+               int index=st.top();
+               st.pop();
+               //i-->nsr
+               //stack->nsl
+               if(st.empty()){
+                   int range=i;//maximum window size
+                   ans[range-1]=max(ans[range-1],arr[index]);
+               }
+               else{
+                   int range=i-st.top()-1;//maximum window size
+                      ans[range-1]=max(ans[range-1],arr[index]);
+               }
+           }
+           st.push(i);
+        }
+        while(!st.empty()){
+            int index=st.top();
+               st.pop();
+               //i-->nsr
+               //stack->nsl
+               if(st.empty()){
+                   int range=n;//maximum window size
+                   ans[range-1]=max(ans[range-1],arr[index]);
+               }
+               else{
+                   int range=n-st.top()-1;//maximum window size
+                      ans[range-1]=max(ans[range-1],arr[index]);
+               }
+        }
+        for(int i=n-2;i>=0;i--){
+            ans[i]=max(ans[i],ans[i+1]);
+        }
+        return ans;
+    }
+};
