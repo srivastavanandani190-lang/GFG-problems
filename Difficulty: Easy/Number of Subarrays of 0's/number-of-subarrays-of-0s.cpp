4f5d8f4e109abbe5a655f58584685dc179a26495
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-07-10 12:51:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of Subarrays of 0's/number-of-subarrays-of-0s.cpp
+++ Difficulty: Easy/Number of Subarrays of 0's/number-of-subarrays-of-0s.cpp
@@ -1,18 +1,16 @@
-// User function Template for C++
-
 long long int no_of_subarrays(int n, vector<int> &arr) {
     // Write your code here.
-    long long int total=0;
-    long long int count=0;
-    for(long long int i=0;i<n;i++){
-        if(arr[i]==0){
-            count++;
-            total+=count;
-        }
-        else{
-            count=0;
+      long long total = 0;
+        long long count = 0;
+
+        for (int num : arr) {
+            if (num == 0) {
+                count++;
+                total += count;
+            } else {
+                count = 0;
+            }
         }
 
-    }
-            return total;
+        return total;
 }
