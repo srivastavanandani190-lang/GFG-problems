b939bc0890d715061bf211b3517bf0a8b987ea06
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-07-10 12:08:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Multiples of 3 or 7/sum-of-multiples-of-3-or-7.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int sum(int n) {
+        // code here
+        int sum=0;
+        for(int i=1;i<n;i++){
+            if(i%3==0 || i%7==0){
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+};
