73c109f7452cc2a4a4f9bcdbe828a3489558e7fc
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-07-10 12:38:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Digits that Divide/count-digits-that-divide.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int evenlyDivides(int n) {
+        int original = n;
+        int count = 0;
+        
+        while (n > 0) {
+            int digit = n % 10;
+            
+            if (digit != 0 && original % digit == 0) {
+                count++;
+            }
+            
+            n = n / 10;
+        }
+        
+        return count;
+    }
+};
