9041eadd054c145e8a63f060d6775906b2e22d98
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-07-23 18:08:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Last index of One/last-index-of-one.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int lastIndex(string s) {
+        int index=-1;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='1'){
+                index=i;
+            }
+        }
+        return index;
+    }
+};
