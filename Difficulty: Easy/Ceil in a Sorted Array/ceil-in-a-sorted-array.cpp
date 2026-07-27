6b252ec7e80e6e3d8c25b82dc5979e218f0a6642
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-07-27 13:20:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Ceil in a Sorted Array/ceil-in-a-sorted-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int findCeil(vector<int>& arr, int x) {
+        // code here
+        int index=-1;
+        for(int i =0;i<arr.size();i++){
+            if(arr[i]>=x){
+                index=i;
+                break;
+            }
+        }
+        return index;
+    }
+};
