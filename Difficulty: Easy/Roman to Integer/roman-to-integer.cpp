c84a6664a5056c34b9fe244e2a7ab9bb7d9b0072
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-07-27 20:20:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Roman to Integer/roman-to-integer.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    int num(char c) {
+        if (c == 'I') return 1;
+        else if (c == 'V') return 5;
+        else if (c == 'X') return 10;
+        else if (c == 'L') return 50;
+        else if (c == 'C') return 100;
+        else if (c == 'D') return 500;
+        return 1000;   // M
+    }
+    int romanToInteger(string &s) {
+        // code here
+            int sum = 0;
+        int index = 0;
+
+        while (index < s.size() - 1) {
+            if (num(s[index]) < num(s[index + 1]))
+                sum -= num(s[index]);
+            else
+                sum += num(s[index]);
+
+            index++;
+        }
+
+        sum += num(s[s.size() - 1]);
+        return sum;
+    }
+};
