b19b2f3117a91785fe30b3df9afa0ce9fc5d84c4
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-07-31 09:47:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count Set Bits/count-set-bits.cpp
+++ Difficulty: Easy/Count Set Bits/count-set-bits.cpp
@@ -1,15 +1,15 @@
 class Solution {
   public:
     int setBits(int n) {
-        // Write Your Code here
-        bitset<32>b(n);
-        string s=b.to_string();
-        int count=0;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='1'){
+        // Code here
+          bitset<32> b(n);
+        string s = b.to_string();
+        int count = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '1') {
                 count++;
             }
         }
-     return count;   
+        return count;
     }
 };
