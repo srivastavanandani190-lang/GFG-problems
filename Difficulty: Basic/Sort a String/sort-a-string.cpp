1b6285529a24de01f7f330ffe235593c98898a63
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-08-14 23:03:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sort a String/sort-a-string.cpp
+++ Difficulty: Basic/Sort a String/sort-a-string.cpp
@@ -2,7 +2,24 @@ class Solution {
   public:
     string sortString(string &s) {
         // code here
-     sort(s.begin(),s.end());
-     return s;
+    //  sort(s.begin(),s.end());
+    //  return s;
+    vector<int>alpha(26,0);
+    for(int i=0;i<s.size();i++){
+        // alpha[s[i]-'a']++;
+        int index=s[i]-'a';
+        alpha[index]++;
+    }
+    
+    string ans;
+    for(int i=0;i<26;i++){
+        char ch=i+'a';
+    while(alpha[i]){
+        ans+=ch;
+        alpha[i]--;
+    }
+        
+    }
+    return ans;
     }
 };
