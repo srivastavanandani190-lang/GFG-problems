1c0f2f585769fa0d94eb08983cde879de1c6f2c9
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-09-01 20:43:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Third Largest/third-largest.cpp
+++ Difficulty: Easy/Third Largest/third-largest.cpp
@@ -1,12 +1,15 @@
-class Solution {
-  public:
-    int thirdLargest(vector<int> &arr) {
-        int n = arr.size();
-        
-        if(n < 3) return -1;
-        
-        sort(arr.begin(), arr.end());
-        
-        return arr[n - 3];
-    }
-};
+ class Solution {
+   public:
+     int thirdLargest(vector<int> &nums) {
+         // If there are fewer than 3 elements, return -1
+         if (nums.size() < 3) {
+             return -1;
+         }
+
+         // Sort the array in descending order
+         sort(nums.rbegin(), nums.rend());
+
+         // Return the 3rd element (which is at index 2)
+         return nums[2];
+     }
+ };
