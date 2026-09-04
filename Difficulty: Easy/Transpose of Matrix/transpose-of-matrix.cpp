32e20ecd33569f6371eab6370c44433b73583ffa
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-09-04 15:22:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Transpose of Matrix/transpose-of-matrix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        // code here
+           int m = matrix.size();
+        int n = matrix[0].size();
+
+        // The transposed matrix will have n rows and m columns
+        vector<vector<int>> result(n, vector<int>(m));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // Switch row and column indices
+                result[j][i] = matrix[i][j];
+            }
+        }
+
+        return result;
+    }
+};
