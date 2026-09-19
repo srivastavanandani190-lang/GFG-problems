32af5d34d94ab44b4b81d84ff4c7a1ce9cde77cc
NANDANI KUMARI <srivastavanandani190@gmail.com>
2026-09-19 16:57:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Natural Number Cubes/sum-of-natural-number-cubes.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    int sumOfSeries(int n) {
+        // code here
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum+=pow(i,3);
+        }
+        return sum;
+    }
+};
